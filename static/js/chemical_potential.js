8f107012d22fc358a4a3c23c4050f75876ad7b04
Luke Lister Brown <lukelbro@gmail.com>
2019-10-24 17:39:44 +0100

    another slider update

--- static/js/chemical_potential.js
+++ static/js/chemical_potential.js
@@ -3,10 +3,12 @@ var container = document.getElementById("chemical-potentials-container");
 canvas.width = container.offsetWidth;
 canvas.height = 300;
 var context = canvas.getContext("2d");
+var spacingWidth = 12;
+var levelWidth = (canvas.width/3) - spacingWidth;
 
 function generateSourceLine(sourceVal) {
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.clearRect(0, 0, levelWidth + 1, canvas.height);
     context.beginPath();
-    context.rect(0, canvas.height-sourceVal, canvas.width , canvas.height);
+    context.rect(0, canvas.height - (sourceVal * canvas.height)/110  , levelWidth, canvas.height);
     context.stroke();
 }
--- static/js/slider.js
+++ static/js/slider.js
@@ -1,6 +1,7 @@
 var slider = document.getElementById("source-slider");
 var output = document.getElementById("source-value");
 output.innerHTML = slider.value;
+generateSourceLine(slider.value);
 
 slider.oninput = function() {
   output.innerHTML = this.value;
