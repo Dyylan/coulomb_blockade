5f2977a3b8a7c90da13a98b40cd0e7dcd4ce1fcf
Dylan Lewis <dylan.lewis@zoho.com>
2019-10-24 17:24:19 +0100

    fixed source slider

--- static/js/slider.js
+++ static/js/slider.js
@@ -1,5 +1,5 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
+var slider = document.getElementById("source-slider");
+var output = document.getElementById("source-value");
 output.innerHTML = slider.value;
 
 slider.oninput = function() {
