72d40a6bc38372080010d2cb40703104fd6c3a7f
Dylan Lewis <dylan.lewis@zoho.com>
2019-10-24 16:51:18 +0100

    conflict resolution

--- static/js/chemical_potential.js
+++ static/js/chemical_potential.js
@@ -1,10 +1,13 @@
 var canvas = document.getElementById("chemical-potentials");
+var container = document.getElementById("chemical-potentials-container");
+canvas.width = container.offsetWidth;
+canvas.height = container.offsetHeight;
 var context = canvas.getContext("2d");
 
 function generateSourceLine(sourceVal) {
     context.clearRect(0, 0, canvas.width, canvas.height);
-    context.beginPath();
-    context.rect(0, canvas.height-sourceVal, 150, canvas.height);
+    context.moveTo(0, canvas.height-sourceVal);
+    context.lineTo(150, canvas.height-sourceVal);
     context.stroke();
 }
 
