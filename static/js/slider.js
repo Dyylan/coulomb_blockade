2b283fe650c75c1786667f31eb1a8484ab9a85ce
Luke Lister Brown <lukelbro@gmail.com>
2019-10-24 22:49:18 +0100

    Added independent drain and source

--- static/js/slider.js
+++ static/js/slider.js
@@ -1,9 +1,19 @@
-var slider = document.getElementById("source-slider");
-var output = document.getElementById("source-value");
-output.innerHTML = slider.value;
-generateSourceLine(slider.value);
+var sliderSource = document.getElementById("source-slider");
+var outputSource = document.getElementById("source-value");
+var sliderDrain = document.getElementById("drain-slider");
+var outputDrain = document.getElementById("drain-value");
+outputSource.innerHTML = sliderSource.value;
+outputDrain.innerHTML = sliderDrain.value;
 
-slider.oninput = function() {
-  output.innerHTML = this.value;
+generateSourceLine(sliderSource.value)
+generateDrainLine(sliderDrain.value);
+
+sliderSource.oninput = function() {
+  outputSource.innerHTML = this.value;
   generateSourceLine(this.value);
+}
+
+sliderDrain.oninput = function() {
+  outputDrain.innerHTML = this.value;
+  generateDrainLine(this.value);
 }
