19f9b63e4354dac9ab65fb8ec38df9e71e4b364f
Luke Lister Brown <lukelbro@gmail.com>
2019-10-25 12:45:42 +0100

    island slider

--- static/js/chemical_potential.js
+++ static/js/chemical_potential.js
@@ -5,11 +5,25 @@ canvas.height = 300;
 var context = canvas.getContext("2d");
 var spacingWidth = 12;
 var levelWidth = (canvas.width/3) - spacingWidth;
+var chargingEnergy = 30;
 
 function generateSourceLine(sourceVal) {
     context.clearRect(0, 0, levelWidth + 1, canvas.height);
     context.beginPath();
     context.rect(0, canvas.height - (sourceVal * canvas.height)/110  , levelWidth, canvas.height);
+    context.stroke();
+}
+
+function generateIslandLine(islandVal) {
+    context.clearRect((levelWidth + spacingWidth) - 1, 0, levelWidth + 2, canvas.height);
+    context.beginPath();
+
+    context.moveTo((  levelWidth + spacingWidth) ,  (islandVal * canvas.height)/110); //U(N) line
+    context.lineTo((2*levelWidth + spacingWidth) ,  (islandVal * canvas.height)/110);
+
+    context.moveTo((  levelWidth + spacingWidth) , ((islandVal - chargingEnergy) * canvas.height)/110); //U(N+1) line
+    context.lineTo((2*levelWidth + spacingWidth) , ((islandVal - chargingEnergy) * canvas.height)/110);
+    
     context.stroke();
 }
 
--- static/js/slider.js
+++ static/js/slider.js
@@ -2,11 +2,15 @@ var sliderSource = document.getElementById("source-slider");
 var outputSource = document.getElementById("source-value");
 var sliderDrain = document.getElementById("drain-slider");
 var outputDrain = document.getElementById("drain-value");
+var sliderIsland = document.getElementById("island-slider");
+var outputIsland = document.getElementById("island-value");
 outputSource.innerHTML = sliderSource.value;
 outputDrain.innerHTML = sliderDrain.value;
+outputIsland.innerHTML = sliderIsland.value;
 
 generateSourceLine(sliderSource.value)
 generateDrainLine(sliderDrain.value);
+generateIslandLine(sliderIsland.value);
 
 sliderSource.oninput = function() {
   outputSource.innerHTML = this.value;
@@ -16,4 +20,9 @@ sliderSource.oninput = function() {
 sliderDrain.oninput = function() {
   outputDrain.innerHTML = this.value;
   generateDrainLine(this.value);
+}
+
+sliderIsland.oninput = function() {
+  outputIsland.innerHTML = this.value;
+  generateIslandLine(this.value);
 }
