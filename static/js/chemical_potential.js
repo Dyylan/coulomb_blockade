42bd51ae5c0021a16da4c84914e248a0245fc49a
Luke Lister Brown <lukelbro@gmail.com>
2019-10-25 14:49:16 +0100

    new dE levels on sliders

--- static/js/chemical_potential.js
+++ static/js/chemical_potential.js
@@ -4,30 +4,56 @@ potentialsCanvas.width = potentialsContainer.offsetWidth;
 var potentialsContext = potentialsCanvas.getContext("2d");
 var spacingWidth = 12;
 var levelWidth = (potentialsCanvas.width/3) - spacingWidth;
-var chargingEnergy = 30;
+var chargingEnergy = 20;
+var quantumEnergy = 5;
+potentialsContext.lineWidth = 2;
 
 function generateSourceLine(sourceVal) {
-    potentialsContext.clearRect(0, 0, levelWidth + 1, potentialsCanvas.height);
+    potentialsContext.clearRect(0, 0, levelWidth + 2, potentialsCanvas.height);
     potentialsContext.beginPath();
     potentialsContext.rect(0, potentialsCanvas.height - (sourceVal * potentialsCanvas.height)/110  , levelWidth, potentialsCanvas.height);
     potentialsContext.stroke();
 }
 
 function generateIslandLine(islandVal) {
-    potentialsContext.clearRect((levelWidth + spacingWidth) - 1, 0, levelWidth + 2, potentialsCanvas.height);
+    potentialsContext.clearRect((levelWidth + spacingWidth) - 2, 0, levelWidth + 3, potentialsCanvas.height);
     potentialsContext.beginPath();
 
     potentialsContext.moveTo((  levelWidth + spacingWidth) ,  (islandVal * potentialsCanvas.height)/110); //U(N) line
     potentialsContext.lineTo((2*levelWidth + spacingWidth) ,  (islandVal * potentialsCanvas.height)/110);
 
-    potentialsContext.moveTo((  levelWidth + spacingWidth) , ((islandVal - chargingEnergy) * potentialsCanvas.height)/110); //U(N+1) line
-    potentialsContext.lineTo((2*levelWidth + spacingWidth) , ((islandVal - chargingEnergy) * potentialsCanvas.height)/110);
+    potentialsContext.moveTo((  levelWidth + spacingWidth) , ((islandVal - chargingEnergy - quantumEnergy) * potentialsCanvas.height)/110); //U(N+1) + dE line
+    potentialsContext.lineTo((2*levelWidth + spacingWidth) , ((islandVal - chargingEnergy - quantumEnergy) * potentialsCanvas.height)/110); 
+
+    potentialsContext.moveTo((  levelWidth + spacingWidth) , ((islandVal - chargingEnergy - quantumEnergy - chargingEnergy) * potentialsCanvas.height)/110); //U(N+2) line
+    potentialsContext.lineTo((2*levelWidth + spacingWidth) , ((islandVal - chargingEnergy - quantumEnergy - chargingEnergy) * potentialsCanvas.height)/110); 
+ 
+  
+    potentialsContext.moveTo((  levelWidth + spacingWidth) , ((islandVal - chargingEnergy - quantumEnergy - chargingEnergy - chargingEnergy - quantumEnergy) * potentialsCanvas.height)/110); //U(N+3) + dE line
+    potentialsContext.lineTo((2*levelWidth + spacingWidth) , ((islandVal - chargingEnergy - quantumEnergy - chargingEnergy - chargingEnergy - quantumEnergy) * potentialsCanvas.height)/110); 
+
+    potentialsContext.moveTo((  levelWidth + spacingWidth) , ((islandVal - chargingEnergy - quantumEnergy - chargingEnergy - chargingEnergy - quantumEnergy - chargingEnergy) * potentialsCanvas.height)/110); //U(N+4) line
+    potentialsContext.lineTo((2*levelWidth + spacingWidth) , ((islandVal - chargingEnergy - quantumEnergy - chargingEnergy - chargingEnergy - quantumEnergy - chargingEnergy) * potentialsCanvas.height)/110); 
     
+    potentialsContext.strokeStyle = '#c58025';
     potentialsContext.stroke();
+
+    potentialsContext.beginPath();
+    potentialsContext.moveTo((  levelWidth + spacingWidth) , ((islandVal - chargingEnergy) * potentialsCanvas.height)/110); //U(N+1) (extra line)
+    potentialsContext.lineTo((2*levelWidth + spacingWidth) , ((islandVal - chargingEnergy) * potentialsCanvas.height)/110); 
+
+    potentialsContext.moveTo((  levelWidth + spacingWidth) , ((islandVal - chargingEnergy - quantumEnergy - chargingEnergy - chargingEnergy) * potentialsCanvas.height)/110); //U(N+3) (extra line)
+    potentialsContext.lineTo((2*levelWidth + spacingWidth) , ((islandVal - chargingEnergy - quantumEnergy - chargingEnergy - chargingEnergy) * potentialsCanvas.height)/110); 
+
+    
+    potentialsContext.strokeStyle = '#8a8a8a';
+    potentialsContext.stroke();
+    
+    potentialsContext.strokeStyle = '#000000';
 }
 
 function generateDrainLine(drainVal) {
-    potentialsContext.clearRect((levelWidth+spacingWidth)*2 - 1, 0, levelWidth + 2, potentialsCanvas.height);
+    potentialsContext.clearRect((levelWidth+spacingWidth)*2 - 2, 0, levelWidth + 3, potentialsCanvas.height);
     potentialsContext.beginPath();
     potentialsContext.rect((levelWidth+spacingWidth)*2, potentialsCanvas.height - (drainVal * potentialsCanvas.height)/110  , levelWidth, potentialsCanvas.height);
     potentialsContext.stroke();
