9a09225804b018ae8f9735764eb6b05b33e94746
Luke Lister Brown <lukelbro@gmail.com>
2019-10-31 16:07:42 +0000

    restored drain slider

--- static/js/slider.js
+++ static/js/slider.js
@@ -1,34 +1,32 @@
 var sliderSource = document.getElementById("source-slider");
 var outputSource = document.getElementById("source-value");
-//var sliderDrain = document.getElementById("drain-slider");
-//var outputDrain = document.getElementById("drain-value");
+var sliderDrain = document.getElementById("drain-slider");
+var outputDrain = document.getElementById("drain-value");
 var sliderIsland = document.getElementById("island-slider");
 var outputIsland = document.getElementById("island-value");
 
-var sliderDrain = 50;
-
 outputSource.innerHTML = sliderSource.value;
-//outputDrain.innerHTML = sliderDrain.value;
+outputDrain.innerHTML = sliderDrain.value;
 outputIsland.innerHTML = sliderIsland.value;
 
 generateSourceLine(sliderSource.value)
-generateDrainLine(sliderSource.value);
+generateDrainLine(sliderDrain.value);
 generateIslandLine(sliderIsland.value);
 
 sliderSource.oninput = function() {
   outputSource.innerHTML = this.value;
   generateSourceLine(this.value);
-  calculateDiamondPosition(this.value, sliderDrain, sliderIsland.value);
+  calculateDiamondPosition(this.value, sliderDrain.value, sliderIsland.value);
 }
 
-//sliderDrain.oninput = function() {
-//  outputDrain.innerHTML = this.value;
-//   generateDrainLine(this.value);
- // calculateDiamondPosition(sliderSource.value, this.value, sliderIsland.value);
-//}
+sliderDrain.oninput = function() {
+  outputDrain.innerHTML = this.value;
+   generateDrainLine(this.value);
+  calculateDiamondPosition(sliderSource.value, this.value, sliderIsland.value);
+}
 
 sliderIsland.oninput = function() {
   outputIsland.innerHTML = this.value;
   generateIslandLine(this.value);
-  calculateDiamondPosition(sliderSource.value, sliderDrain, this.value);
+  calculateDiamondPosition(sliderSource.value, sliderDrain.value, this.value);
 }
