2ae93555534e73ca9a94d7f70953e00829bab220
Luke Lister Brown <lukelbro@gmail.com>
2019-10-31 15:41:29 +0000

    potential level spacing and labels

--- static/js/chemical_potential.js
+++ static/js/chemical_potential.js
@@ -8,13 +8,14 @@ var chargingEnergy = 20;
 var quantumEnergy = 5;
 var diamondCount = 4;
 potentialsContext.lineWidth = 2;
-potentialsContext.font = "10px Arial";
+potentialsContext.font = "15px Courier New";
 
 function generateSourceLine(sourceVal) {
-    potentialsContext.clearRect(0, 0, levelWidth + 2, potentialsCanvas.height);
+    potentialsContext.clearRect(0, 0, levelWidth + 3, potentialsCanvas.height);
     potentialsContext.beginPath();
-    potentialsContext.rect(0, potentialsCanvas.height - (sourceVal * potentialsCanvas.height)/110  , levelWidth, potentialsCanvas.height);
+    potentialsContext.rect(2, potentialsCanvas.height - (sourceVal * potentialsCanvas.height)/110  , levelWidth, potentialsCanvas.height);
     potentialsContext.stroke();
+    potentialsContext.fillText("μ(s)", levelWidth/2 - 15, (potentialsCanvas.height - (sourceVal * potentialsCanvas.height)/110) + 15);
 }
 
 function generateDrainLine(drainVal) {
@@ -22,10 +23,11 @@ function generateDrainLine(drainVal) {
     potentialsContext.beginPath();
     potentialsContext.rect((levelWidth+spacingWidth)*2, potentialsCanvas.height - (drainVal * potentialsCanvas.height)/110  , levelWidth, potentialsCanvas.height); 
     potentialsContext.stroke();
+    potentialsContext.fillText("μ(d)", (levelWidth + spacingWidth) * 2 + levelWidth/2 -15, potentialsCanvas.height - (drainVal * potentialsCanvas.height)/110 + 15);
 }
 
 function generateIslandLine(islandVal) {
-    potentialsContext.clearRect((levelWidth + spacingWidth) - 2, 0, levelWidth + 3, potentialsCanvas.height);
+    potentialsContext.clearRect((levelWidth + spacingWidth) - 2, -5, levelWidth + 3, potentialsCanvas.height+10);
     var i;
     for (i=1; i < diamondCount+1; i++){
         drawEnergyLine(islandVal, i);
@@ -45,12 +47,24 @@ function drawEnergyLine(islandVal, i){
     potentialsContext.stroke();
     potentialsContext.strokeStyle = "#000000";
     
+    var numElectrons = i-3;
+    if(numElectrons<0)
+        potentialsContext.fillText("μ(N" + numElectrons + ")", lineX1, lineHeight - 10);
+    if(numElectrons==0)
+        potentialsContext.fillText("μ(N)", lineX1, lineHeight - 10);
+    if(numElectrons>0)
+        potentialsContext.fillText("μ(N+" + numElectrons + ")", lineX1, lineHeight - 10);
+    
     if(i%2 == 0){ //Virtual Energy Lines
         potentialsContext.beginPath();
         potentialsContext.moveTo(lineX1 , lineHeight + chargingEnergy);
         potentialsContext.lineTo(lineX2 , lineHeight + chargingEnergy);
         potentialsContext.strokeStyle = '#8a8a8a';
+        potentialsContext.lineWidth = 1;
+        potentialsContext.setLineDash([3, 8]);
         potentialsContext.stroke();
         potentialsContext.strokeStyle = "#000000";
+        potentialsContext.lineWidth = 2;
+        potentialsContext.setLineDash([]);
     }
 } 
